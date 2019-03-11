6390253210867b1b34ce353fa219ba3f637b75f8
Kelly Sousa <kellysousa1920@gmail.com>
2019-03-11 15:05:36 -0700

    edit and delete buttons appear and then errors out but events are printing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Jumbotron} from 'react-bootstrap';
+import {ListGroup} from 'react-bootstrap';
 import './App.css';
-import './keys.js'
+import './keys.js';
+// import EventList from './EventList.js';
 
+
+const API = '/events/'
+ 
 
 const Header = (props) => {
   return ( 
@@ -13,122 +18,133 @@ const Header = (props) => {
   )
 }
 
-// class EventItem extends React.Component {
-
-//   // state = {
-//   //   events: this.getSingleEvent
-//   // };
-// }
-  // extends React.Component {
-//   state = {
-//     events: this.getEvents
-//   };
-
-//   var events = getEvents();
-//   console.log("let's go ", events);
-//   }
-
-//   function getEvents() {
-//     let xhr = new XMLHttpRequest()
-//     xhr.onreadystatechange = function () {
-//       if (xhr.readyState === 4) {
-//     //     return xhr.responseText
-//     //   }
-//     // }
-//       xhr.open('GET', 'lvh.me:3006/events/')
-//       xhr.send();
-//       function sendAJAX(){
-//         xhr.send();
-//       }
-//   }
-//   render (){
-//     return (
-//       <ul className="list">
-//         {/* 1. call API to recieve a list of events... attempted above  */}
-//         {/* 2. for each event on that list create an event item class... then map */}
-//         {/* // info you want to get from eventful
-//           // <li>
-//             // <div>
-//                 //<h3>Name</h3>
-//                 //<address>City, State</address>
-//                 //<span>Type of Event</span>
-//             // </div>
-//                         // < edit button />
-//                         // < del button />  
-//           // </li>
-//         // } */} 
-//       </ul>
-//     )
-//   }
-// }
-
-
-
-
- class App extends React.Component {
-  
-  constructor() {
-    super();
-    this.state = {
-      myAPIData: [],
-      eventfulData: []
-    };
-  } 
-
-  componentDidMount() {
-    fetch('/events/')
-      .then(response => response.json())
-      .then(responseData =>{
-        this.setState({myAPIData: responseData});
-      })
-      .catch(error =>{
-        console.log('Error FETCHING and parsing data from YOUR API juheard', error);
-      });
-  }
-  
-
-  componentDidMount() {
-    fetch('/events/')
-      .then(response => response.json())
-      .then(responseData =>{
-        this.setState({eventfulData: responseData});
-      })
-      .catch(error =>{
-        console.log('Error FETCHING and parsing data from EVENTFUL API juheard', error);
-      });
-  }
-  
-  render() {
-    console.log(this.state.eventfulData)
+const EventList = (props) => {
     return (
-      <div className="App">
-        <Header
-          title="Eventonica!"
-          siteDescript="This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
-        />
-      </div>
-    );
+      <ListGroup>
+        <div className="allEvents">
+          <span className="event-info">
+              <ul>
+                <button className="edit-event" onClick={ () => props.editEvent(props.id)}>EDIT</button>
+                  {props.name}
+                <button className="remove-event" onClick={ () => props.removeEvent(props.id)}>✖</button>
+                  {props.name}
+              </ul>
+          </span>
+        </ div>
+      </ListGroup>
+    )
   }
-}
 
-// export default class App extends Component {
 
-//   render() { 
-//     return (
-//       <div>
-//         <div className="main-header">
-//           <div className="inner">
-//             <h1 className="main-title">GifSearch</h1>
-//             <SearchForm />      
-//           </div>   
-//         </div>    
-//         <div className="main-content">
-//           <GifList />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        events: [],
+      };
+    }
 
+
+
+    loadEvents() {
+      fetch(API, {
+        headers: {"content-type": "application/json"}
+      })
+        .then(resp => {
+          if (!resp.ok) {
+            if (resp.status >= 400 && resp.status < 500) {
+              return resp.json().then(data => {
+                let err = {
+                  errorMessage: data.message
+                };
+                throw err;
+              });
+            } else {
+              let err = {
+                errorrMessage: "Error FETCHING and parsing data from YOUR API juheard"
+              };
+              throw err;
+            }
+          }
+          return resp.json();
+        })
+        .then(events => this.setState({
+          events
+        }));
+    }
+
+
+
+    // componentDidMount() {
+    //   fetch('/events')
+    //     .then(response => response.json())
+    //     .then(responseData =>{
+    //       this.setState({API: responseData});
+    //     })
+    //     .catch(error =>{
+    //       console.log('Error FETCHING and parsing data from YOUR API juheard caw caw', error);
+    //     });
+    // }
+    
+
+
+    // loadEvents() {
+    //   fetch(API)
+    //     .then(resp => {
+    //       if (!resp.ok) {
+    //         if (resp.status >= 400 && resp.status < 500) {
+    //           return resp.json().then(data => {
+    //             let err = {
+    //               errorMessage: data.message
+    //             };
+    //             throw err;
+    //           });
+    //         } else {
+    //           let err = {
+    //             errorrMessage: "Error FETCHING and parsing data from YOUR API juheard"
+    //           };
+    //           throw err;
+    //         }
+    //       }
+    //       return resp.json();
+    //     })
+    //     .then(events => this.setState({
+    //       events
+    //     }));
+    // }
+
+
+    componentDidMount() {
+      this.loadEvents();
+    }
+    // handleRemoveEvents = (id) => {
+    //   this.setState( prevState => {
+    //       return {
+    //         events: prevState.events.filter( e => e.id !== id)
+    //       }
+    //   });
+    // }
+
+    render() {
+        return ( 
+          <div className ="App">
+
+          <Header title = "Eventonica!"
+          siteDescript = "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information." 
+          />
+
+          {this.state.events.map(events => ( 
+              <EventList 
+              name = {events.name}
+              city = {events.city}
+              state = {events.state}
+              desc = {events.description}
+              />
+            ))
+          }
+        </div>
+        )
+    }
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import App from './App';
+// import axios from 'axios';
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 
 
